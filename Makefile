e78d8777a0618ea29ee5d13fbb35a54beb839a87
Manon louise Minkjan <mminkjan@student.codam.nl>
2021-05-07 19:32:55 +0200

    Opengl integrated without glew lib

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jesmith <jesmith@student.codam.nl>           +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/08/28 15:14:36 by jesmith       #+#    #+#                  #
-#    Updated: 2021/05/07 17:16:20 by mminkjan      ########   odam.nl          #
+#    Updated: 2021/05/07 19:32:45 by mminkjan      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,9 +17,11 @@ RED = $(shell printf "\033[0;31m")
 NAME = scop
 FLAGS = -Wall -Wextra -Werror
 LIBFT = libft
+GLEW = glew
+GLEW_INCL = glew/include/GL/
 SDL = sdl
-SDL_FLAGS = `sdl2-config --cflags --libs`
-LIBS = -L $(LIBFT) -lft -lSDL2_mixer
+SDL_FLAGS = `sdl2-config --cflags --libs`  -framework OpenGL -lGLEW -I GLEW_INCL 
+LIBS = -L $(LIBFT) -lft -lSDL2_mixer $(GLEW_LIB)
 
 SRCS_DIR = srcs/
 SRCS_FILES = main render SDL_init
@@ -64,7 +66,7 @@ fclean: clean
 re: fclean all
 
 add: fclean
-	@git add $(LIBFT) $(HEADERS) $(ADD_FILES) $(SDL)
+	@git add $(LIBFT) $(HEADERS) $(ADD_FILES) $(SDL) 
 	$(C_FILES) @git status
 
 push:
--- includes/scop.h
+++ includes/scop.h
@@ -6,14 +6,16 @@
 /*   By: mminkjan <mminkjan@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/06 14:51:59 by mminkjan      #+#    #+#                 */
-/*   Updated: 2021/05/07 17:12:53 by mminkjan      ########   odam.nl         */
+/*   Updated: 2021/05/07 19:28:34 by mminkjan      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCOP_H
 # define SCOP_H
 
+# include "../glew/include/GL/glew.h"
 # include "../sdl/includes/SDL.h"
+# include "../sdl/includes/SDL_opengl.h"
 # include "../libft/libft.h"
 
 # include <unistd.h>
@@ -22,8 +24,8 @@
 # include <stdbool.h>
 
 # define WINDOW_NAME "Scop"
-# define WIDTH	900
-# define HEIGHT 600
+# define WIDTH	640
+# define HEIGHT 480
 
 # define TRUE	1
 # define FALSE	0
@@ -33,11 +35,12 @@ typedef struct s_cop {
 	SDL_Window	*window;
 	SDL_Surface *surface;
 	SDL_Event	*event;
+	
 	bool		running;
 }				t_cop;
 
 int 	main(void);
-void	SDL_init(t_cop *scop);
+void	SDL_GL_init(t_cop *scop);
 void	render(t_cop *scop);
 void	scop_return_error(t_cop *cop, const char *message);
 
